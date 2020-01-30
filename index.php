95f02ae4b3c2884b0f4497c1deaa5266f232da5c
Tom Hoevers <tomleonhoevers@gmail.com>
2020-01-30 10:34:43 +0100

    First commit, added XAMPP htdocs entirely

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template File Doc Comment
+ * 
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+header('Location: '.$uri.'/dashboard/');
+exit;
+?>
+Something is wrong with the XAMPP installation :-(
---
+++ newsite/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template File Doc Comment
+ * 
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/newsite
+ */
+    echo 'Hello World!';
+?>
